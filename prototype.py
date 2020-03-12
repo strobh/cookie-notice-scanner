5cd08d95da6999921330cae33c97bc9c44112540
Tobias Heckel <developer@tobias-heckel.de>
2020-03-12 11:51:51 +0100

    added functionality to check whether node is visible or not.

--- prototype.py
+++ prototype.py
@@ -33,10 +33,12 @@ class WebpageResult:
             'url': request_url,
         })
 
-    def add_response(self, requested_url, mime_type):
+    def add_response(self, requested_url, status, mime_type, headers):
         self.responses.append({
             'url': requested_url,
+            'status': status,
             'mime_type': mime_type,
+            'headers': headers,
         })
 
     def add_screenshot(self, name, screenshot):
@@ -90,15 +92,24 @@ class Crawler:
         # get root node of document, is needed to be sure that the DOM is loaded
         root_node = self.tab.DOM.getDocument()
 
-        #self.find_cookie_notice_by_rules()
-        #self.find_cookie_notice_by_cmp()
+        cookie_notices_node_ids, rules = self.find_cookie_notice_by_rules()
+        print(rules)
+        cookie_notices_node_ids = [node_id for node_id in cookie_notices_node_ids if self.is_node_visible(node_id)]
+        #self.is_cmp_function_defined()
 
         #self.search_dom_for_cookie()
 
-        self.take_screenshot('before')
-        self.tab.Input.emulateTouchFromMouseEvent(type="mouseWheel", x=1, y=1, button="none", deltaX=0, deltaY=-100)
-        self.tab.wait(0.1)
-        self.take_screenshot('after')
+        #self.take_screenshot('before')
+        #self.tab.Input.emulateTouchFromMouseEvent(type="mouseWheel", x=1, y=1, button="none", deltaX=0, deltaY=-100)
+        #self.tab.wait(0.1)
+        #self.take_screenshot('after')
+
+        color_content = {'r': 152, 'g': 196, 'b': 234, 'a': 0.5}
+        color_padding = {'r': 184, 'g': 226, 'b': 183, 'a': 0.5}
+        hightlightConfig = {'contentColor': color_content, 'paddingColor': color_padding}
+        for index, node_id in enumerate(cookie_notices_node_ids):
+            self.tab.Overlay.highlightNode(highlightConfig=hightlightConfig, nodeId=node_id)
+            self.take_screenshot('overlay-' + str(index))
 
         # stop and close the tab
         self.tab.stop()
@@ -128,6 +139,7 @@ class Crawler:
         # enable DOM and Runtime
         tab.DOM.enable()
         tab.Runtime.enable()
+        tab.Overlay.enable()
 
         return tab
 
@@ -141,6 +153,7 @@ class Crawler:
         there can still be connection issues.
         """
         url = request['url']
+        #pprint(request)
         self.result.add_request(request_url=url)
 
     def _event_response_received(self, response, **kwargs):
@@ -151,7 +164,9 @@ class Crawler:
         """
         url = response['url']
         mime_type = response['mimeType']
-        self.result.add_response(requested_url=url, mime_type=mime_type)
+        status = response['status']
+        headers = response['headers']
+        self.result.add_response(requested_url=url, status=status, mime_type=mime_type, headers=headers)
 
     def _event_load_event_fired(self, timestamp, **kwargs):
         """Will be called when the page sends an load event.
@@ -182,24 +197,27 @@ class Crawler:
         search_object = self.tab.DOM.performSearch(query="//*[contains(translate(text(), 'COKIE', 'cokie'), 'cookie')]")
         node_ids = self.tab.DOM.getSearchResults(searchId=search_object.get('searchId'), fromIndex=0, toIndex=int(search_object.get('resultCount')))
         node_ids = node_ids['nodeIds']
-        pprint(node_ids)
 
         # resume execution of scripts
         self.tab.Emulation.setScriptExecutionDisabled(value=False)
+        return node_ids
 
     def find_cookie_notice_by_rules(self):
         results = []
+        rules = []
         root_node_id = self.tab.DOM.getDocument().get('root').get('nodeId')
-        for rule in self._rules:
-            if self._is_rule_applicable(rule, self.result.hostname):
-                node_ids = tab.DOM.querySelectorAll(nodeId=root_node_id, selector=rule.selector.get('value'))
+        for rule in self.abp_filter._rules:
+            if self.abp_filter._is_rule_applicable(rule, self.result.hostname):
+                node_ids = self.tab.DOM.querySelectorAll(nodeId=root_node_id, selector=rule.selector.get('value'))
                 node_ids = node_ids['nodeIds']
                 if len(node_ids) > 0:
                     results = results + node_ids
+                    rules = rules + [rule * len(node_ids)]
 
         self.result.set_cookie_notice_by_rules(results)
+        return results, rules
 
-    def find_cookie_notice_by_cmp(self):
+    def is_cmp_function_defined(self):
         result = self.tab.Runtime.evaluate(expression="typeof window.__cmp !== 'undefined'").get('result')
         return result.get('value')
 
@@ -207,6 +225,40 @@ class Crawler:
         pass
         #getEventListeners()
         # https://developers.google.com/web/tools/chrome-devtools/console/utilities?utm_campaign=2016q3&utm_medium=redirect&utm_source=dcc#geteventlistenersobject
+
+    def is_node_visible(self, node_id):
+        # Source: https://stackoverflow.com/a/41698614
+        js_function = """
+            function() {
+                elem = this;
+                if (!(elem instanceof Element)) throw Error('DomUtil: elem is not an element.');
+                const style = getComputedStyle(elem);
+                if (style.display === 'none') return false;
+                if (style.visibility !== 'visible') return false;
+                if (style.opacity < 0.1) return false;
+                if (elem.offsetWidth + elem.offsetHeight + elem.getBoundingClientRect().height +
+                    elem.getBoundingClientRect().width === 0) {
+                    return false;
+                }
+                const elemCenter   = {
+                    x: elem.getBoundingClientRect().left + elem.offsetWidth / 2,
+                    y: elem.getBoundingClientRect().top + elem.offsetHeight / 2
+                };
+                if (elemCenter.x < 0) return false;
+                if (elemCenter.x > (document.documentElement.clientWidth || window.innerWidth)) return false;
+                if (elemCenter.y < 0) return false;
+                if (elemCenter.y > (document.documentElement.clientHeight || window.innerHeight)) return false;
+                let pointContainer = document.elementFromPoint(elemCenter.x, elemCenter.y);
+                do {
+                    if (pointContainer === elem) return true;
+                    if (!pointCointainer) return false;
+                } while (pointContainer = pointContainer.parentNode);
+                return false;
+            }"""
+
+        remote_object_id = self.tab.DOM.resolveNode(nodeId=node_id).get('object').get('objectId')
+        result = self.tab.Runtime.callFunctionOn(functionDeclaration=js_function, objectId=remote_object_id, silent=True).get('result')
+        return result.get('value')
 
 
 class AdBlockPlusFilter:
